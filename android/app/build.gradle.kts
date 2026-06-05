144b65869e5287719aa65dd094bc79dab6471f5f
ubaiddevplus <ubaiddevplus>
2026-06-05 18:30:01 +0000

    Completely rename project to CalcFlow

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.my_calculator"
+        applicationId = "com.ubaiddevplus.calcflow"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
